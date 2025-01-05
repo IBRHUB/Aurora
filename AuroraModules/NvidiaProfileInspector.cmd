8c3547a6a41029ccf09e65905ef12b23c26c7d99
IBRAHIM <102235503+IBRHUB@users.noreply.github.com>
2025-01-05 15:37:58 +0300

    Update NvidiaProfileInspector.cmd

--- AuroraModules/NvidiaProfileInspector.cmd
+++ AuroraModules/NvidiaProfileInspector.cmd
@@ -25,38 +25,8 @@ Title Aurora NVIDIA Profile Inspector
 :: ============================================================
 
 
-:: NVIDIA Inspector Profile
-echo Applying NVIDIA Inspector Profile
-
-REM Download NVIDIA Profile Inspector
-curl -g -k -L -# -o "%temp%\nvidiaProfileInspector.zip" "https://github.com/Orbmu2k/nvidiaProfileInspector/releases/latest/download/nvidiaProfileInspector.zip"
-if errorlevel 1 (
-    echo Failed to download NVIDIA Profile Inspector.
-    pause
-    goto b
-)
-
-REM Extract the downloaded ZIP
+curl -g -k -L -# -o "%temp%\nvidiaProfileInspector.zip" "https://github.com/Orbmu2k/nvidiaProfileInspector/releases/latest/download/nvidiaProfileInspector.zip" > NUL 2>&1
 powershell -NoProfile Expand-Archive '%temp%\nvidiaProfileInspector.zip' -DestinationPath '%~dp0AuroraNvidia\NvidiaProfileInspector\' -Force
-if errorlevel 1 (
-    echo Failed to extract NVIDIA Profile Inspector.
-    pause
-    goto b
-)
 
-REM Download Aurora profiles
-curl -g -k -L -# -o "%~dp0AuroraNvidia\NvidiaProfileInspector\AuroraOFF.nip" "https://raw.githubusercontent.com/IBRHUB/Aurora/main/AuroraModules/AuroraOFF.nip"
-if errorlevel 1 (
-    echo Failed to download AuroraOFF.nip.
-    pause
-    goto b
-)
-
-curl -g -k -L -# -o "%~dp0AuroraNvidia\NvidiaProfileInspector\AuroraON.nip" "https://raw.githubusercontent.com/IBRHUB/Aurora/main/AuroraModules/AuroraON.nip"
-if errorlevel 1 (
-    echo Failed to download AuroraON.nip.
-    pause
-    goto b
-)
-:b
-exit \b
+curl -g -k -L -# -o "%~dp0AuroraNvidia\NvidiaProfileInspector\AuroraOFF.nip" "https://raw.githubusercontent.com/IBRHUB/Aurora/main/AuroraModules/AuroraOFF.nip" > NUL 2>&1
+curl -g -k -L -# -o "%~dp0AuroraNvidia\NvidiaProfileInspector\AuroraON.nip" "https://raw.githubusercontent.com/IBRHUB/Aurora/main/AuroraModules/AuroraON.nip" > NUL 2>&1
